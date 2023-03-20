fe5bc259afa1af2055e9624a3298d50955067e24
diazcalabuigpau@fpllefia.com <diazcalabuigpau@fpllefia.com>
2023-03-21 00:36:24 +0100

    porfin?

--- src/components/fichaAlumno.vue
+++ src/components/fichaAlumno.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-2 offset-1 px-4">
       <div class="card carta shadow" >
-        <router-link :to="{name:'card', params:{id:persona.id}}"><img class="card-img-top imagen_card" :src='"/public/img/"+persona.photo' :alt='persona.nombre'></router-link>
+        <router-link :to="{name:'card', params:{id:persona.id}}"><img class="card-img-top imagencarta" :src='"/public/img/"+persona.photo' :alt='persona.nombre'></router-link>
         <p class="text-center pt-3 ">{{persona.nombre}}</p>
       </div>
     </div>
--- src/components/fichaProfesor.vue
+++ src/components/fichaProfesor.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-2 offset-1 px-4">
       <div class="card carta shadow">
-        <img class="cardProfesor" :src='"/public/img/"+persona.photo' :alt='persona.nombre'>
+        <img class="cartaProfesor" :src='"/public/img/"+persona.photo' :alt='persona.nombre'>
         <p class="text-center pt-3">{{persona.nombre}}</p>
       </div>
     </div>
